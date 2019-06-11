b0a08aafaf0bc3463dde127f8e808f5c9651143e
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-06-10 19:48:27 -0500

    Added skeleton of modal

--- js/about.js
+++ js/about.js
@@ -54,28 +54,54 @@ const contactInfo = {
 const idEmail = "#resume-contact-email";
 const idPhone = "#resume-contact-phone";
 
-const liToForm = (keyId, message) => {
-  let html = `<div>` +
-    `<form action="#" method="post">` +
-      `<label for="${keyId}">${message}</label>` +
-      `<input id="${keyId}" type=\"text\">` +
-      `<input id="${keyId}-submit" type=\"submit\" value=\"I am not a bot\">`
-    "</form></div>";
-  return html;
+// Get the modal
+const modalEmail = document.getElementById("myModal");
+console.assert(modalEmail !== null);
+
+// Get the button that opens the modal
+const btnEmail = document.getElementById("resume-contact-email");
+console.assert(btnEmail !== null);
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btnEmail.onclick = function() {
+  modalEmail.style.display = "block";
 }
 
-$(idEmail).on("click", (event) => {
-  event.preventDefault();
-  $(idEmail).html(liToForm("email-key", "Please enter the number \"eleven\""));
-  $("#email-key-submit").on("click", (event) => {
-    event.preventDefault();
-    console.log(event);
-  });
-  //$(idEmail).html(decrypt(contactInfo.email, key));
-});
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modalEmail.style.display = "none";
+}
 
-$(idPhone).on("click", (event) => {
-  event.preventDefault();
-  let key = dialog("Enter the number \"eleven\"");
-  $(idPhone).html(decrypt(contactInfo.phone, key));
-});
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalEmail) {
+    modalEmail.style.display = "none";
+  }
+}
+
+// const liToForm = (keyId, message) => {
+//   let html =
+//     `<div>` +
+//       `<label for="${keyId}">${message}</label>` +
+//     `</div>` +
+//     `<div>` +
+//       `<input id="${keyId}" type=\"number\" step=\"1\" value=\"I am not a bot\">` +
+//     `</div>`;
+//   return html;
+// }
+//
+// $(idEmail).on("click", (event) => {
+//   $(idEmail).html(liToForm("email-key", "Please enter the number \"eleven\""));
+//   $("#email-key").on("click", (event) => {
+//     console.log(event);
+//   });
+//   //$(idEmail).html(decrypt(contactInfo.email, key));
+// });
+//
+// $(idPhone).on("click", (event) => {
+//   let key = dialog("Enter the number \"eleven\"");
+//   $(idPhone).html(decrypt(contactInfo.phone, key));
+// });
