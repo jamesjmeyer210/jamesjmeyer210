296b8fc5a889cc10deae430ad1fced785387996b
james meyer <jamesjmeyer@gmail.com>
2020-01-17 14:37:38 -0600

    working through rendering text

--- js/index.js
+++ js/index.js
@@ -1,14 +1,40 @@
 (function(){
     
-    var i = 0;
-    var txt = 'Loading components...';
-    var speed = 50;
+    const speed = 50;
 
-    const typeWriter = () => {
-        if (i < txt.length) {
-            document.getElementById("terminal-content").innerHTML += txt.charAt(i);
+    const typeWriter = (str, i) => {
+        if (i < str.length) {
+            document.getElementById("terminal").innerHTML += str.charAt(i);
             i++;
             setTimeout(typeWriter, speed);
         }
     }
-}());
+	
+	const txt = {
+		startup: [
+			'Loading components...',
+			'index.html',
+			'terminal.css',
+			'index.js',
+			'Component loaded succesfully!'
+		],
+		header: [
+			'. ___[ James Meyer ]___ .',
+			'. ___[ Software Engineer ]___ .'
+		],
+	}
+	
+	console.log(txt);
+	
+	const renderTxt = (lines) => {
+		console.log(lines);
+		lines.forEach((line) => {
+			console.log(line);
+			let i = 0;
+			typeWriter(line, i);
+		});
+	}
+	
+	renderTxt(txt.startup);
+	renderTxt(txt.header);
+})();
