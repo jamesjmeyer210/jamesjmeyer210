e231ed41cc236b6628f552ed136ef58ea606a765
James <jamesjmeyer210@gmail.com>
2020-02-03 14:40:35 -0600

    adding uncommitted changes

--- js/index.js
+++ js/index.js
@@ -1,40 +1,24 @@
 (function(){
     
-    const speed = 50;
+	const output = 'Loading components...' +
+		'index.html' +
+		'terminal.css' +
+		'index.js' +
+		'Component loaded succesfully!' +
+		'. ___[ James Meyer ]___ .' +
+		'. ___[ Software Engineer ]___ .';
 
-    const typeWriter = (str, i) => {
-        if (i < str.length) {
-            document.getElementById("terminal").innerHTML += str.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-        }
-    }
-	
-	const txt = {
-		startup: [
-			'Loading components...',
-			'index.html',
-			'terminal.css',
-			'index.js',
-			'Component loaded succesfully!'
-		],
-		header: [
-			'. ___[ James Meyer ]___ .',
-			'. ___[ Software Engineer ]___ .'
-		],
+	var i = 0;
+	var speed = 50;
+
+	function typeWriter() {
+		if (i < output.length) {
+			document.getElementById("terminal").innerHTML += output.charAt(i);
+			i++;
+			setTimeout(typeWriter, speed);
+		}
 	}
-	
-	console.log(txt);
-	
-	const renderTxt = (lines) => {
-		console.log(lines);
-		lines.forEach((line) => {
-			console.log(line);
-			let i = 0;
-			typeWriter(line, i);
-		});
-	}
-	
-	renderTxt(txt.startup);
-	renderTxt(txt.header);
+
+	typeWriter();
+
 })();
