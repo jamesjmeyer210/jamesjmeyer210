172f60e4018573999f20a9712bbfe9941b568b95
James <jamesjmeyer210@gmail.com>
2020-02-04 11:47:21 -0600

    Resume modals work

--- js/resume.js
+++ js/resume.js
@@ -1,4 +1,4 @@
-(function(){
+function resume() {
   // convert a string to Uint8Array
   const stringToUint8Array = (str) => {
     if(typeof(str) != "string"){
@@ -49,36 +49,44 @@
     email: "ulxp~uxp\u0084p}=<;Krxltw9nzx",
     phone: "?AD8??=8BD>=",
   };
+  
   // Get the modal
   const modalEmail = document.getElementById("modal-email");
   const modalPhone = document.getElementById("modal-phone");
   console.assert(modalEmail !== null && modalPhone !== null);
+  
   // Get the button that opens the modal
   const btnEmail = document.getElementById("resume-contact-email");
   const btnPhone = document.getElementById("resume-contact-phone");
   console.assert(btnEmail !== null && btnPhone !== null);
+  
   // Get the <span> element that closes the modal
   let closeEmail = document.getElementsByClassName("close")[0];
   let closePhone = document.getElementsByClassName("close")[1];
   console.assert(closeEmail !== null && closePhone !== null);
+  
   // When the user clicks on the button, open the modal
   btnEmail.onclick = () => {
     document.getElementById("modal-email-val")
       .innerText = decrypt(contactInfo.email, 11);
     modalEmail.style.display = "block";
   }
+  
   btnPhone.onclick = () => {
     document.getElementById("modal-phone-val")
       .innerText = decrypt(contactInfo.phone, 11);
     modalPhone.style.display = "block";
   }
+  
   // When the user clicks on <span> (x), close the modal
   closeEmail.onclick = () => {
     modalEmail.style.display = "none";
   }
+  
   closePhone.onclick = () => {
     modalPhone.style.display = "none";
   }
+  
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = (event) => {
     if (event.target == modalEmail) {
@@ -89,4 +97,4 @@
     }
   }
 
-})();
+};
